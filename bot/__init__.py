0d106186cd5c7de339780e217f1f98ed071d7a1d
Sureshssd77 <sureshjaat33772@gmail.com>
2025-01-03 15:49:46 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,13 +11,13 @@ from logging.handlers import RotatingFileHandler
 from .translation import Translation
 
 # Change Accordingly While Deploying To A VPS
-APP_ID = int(os.environ.get("APP_ID"))
+APP_ID = int(os.environ.get("22506926"))
 
-API_HASH = os.environ.get("API_HASH")
+API_HASH = os.environ.get("34fe7b7d19572aae39c6db80e151d9f7")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("8004078816:AAFpANlxyG1emrSLYYuJXgEecNGJIvme78c")
 
-DB_URI = os.environ.get("DB_URI")
+DB_URI = os.environ.get("mongodb+srv://dilipdewasi7759:<NMzFkZqWZdi1GDG4>@ssdmovies.2nqad.mongodb.net/?retryWrites=true&w=majority&appName=ssdmovies")
 
 USER_SESSION = os.environ.get("USER_SESSION")
 
